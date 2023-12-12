644bdc57f5660a6ea2fd66f4eaec1f435c59cdea
devisree26 <113516800+devisree26@users.noreply.github.com>
2023-12-12 15:21:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import Portfolio from './Portfolio';
+import './style.css'; // Import your CSS file
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
